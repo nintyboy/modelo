e35ae3d94df2d38ce31e1a38b18e34d060be3db7
Tyrell James <nintyboy@gmail.com>
2023-02-13 17:31:14 +0000

    Inital Commit

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -3,6 +3,7 @@ import { NavigationProvider } from './navigation'
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
+    //TODO: Add Firebase Provider Here: 
     <NavigationProvider>
       <Dripsy>{children}</Dripsy>
     </NavigationProvider>
