0e4b7ae160d22e90b06def5fc940d08441c696f0
Tyrell James <nintyboy@gmail.com>
2023-02-21 10:46:52 +0000

    Added Initial Firebase setup to Next - Side

--- packages/app/provider/index.web.tsx
+++ packages/app/provider/index.web.tsx
@@ -1,16 +1,21 @@
 import { Dripsy } from './dripsy'
+import { FirebaseAppShell } from './firebase/FirebaseAppShell'
 import { NavigationProvider } from './navigation'
+import config from './config';
 
 export function Provider({ children }: { children: React.ReactNode }) {
+  const {firebase} = config;
   return (
    //TODO: Seperate A provider for Web and Mobile `index.web.tsx`
    //TODO: Set up a firebase package and initialise FB
    //TODO: Set up a configuration file with the FB Credentials attached to NEXT_ENV_{CREDNAME}
    //TODO: Add Local / Staging and Productions values
    //TODO: Test by enabling debugView
+   <FirebaseAppShell config={firebase}>
     <NavigationProvider>
       <>Web</>
       <Dripsy>{children}</Dripsy>
     </NavigationProvider>
+  </FirebaseAppShell>
   )
 }
