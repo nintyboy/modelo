18f4e689fb8937a6ccccd7fb8a7f7e9d94a103a6
Tyrell James <nintyboy@gmail.com>
2023-02-21 10:01:38 +0000

    Seperated Web & Mobile Provider

---
+++ packages/app/provider/index.web.tsx
@@ -0,0 +1,16 @@
+import { Dripsy } from './dripsy'
+import { NavigationProvider } from './navigation'
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  return (
+   //TODO: Seperate A provider for Web and Mobile `index.web.tsx`
+   //TODO: Set up a firebase package and initialise FB
+   //TODO: Set up a configuration file with the FB Credentials attached to NEXT_ENV_{CREDNAME}
+   //TODO: Add Local / Staging and Productions values
+   //TODO: Test by enabling debugView
+    <NavigationProvider>
+      <>Web</>
+      <Dripsy>{children}</Dripsy>
+    </NavigationProvider>
+  )
+}
