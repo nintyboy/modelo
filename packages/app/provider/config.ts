d751a7edd1af07fbb0fb8060c7a27e553d655be5
Tyrell James <nintyboy@gmail.com>
2023-03-10 15:30:13 +0000

    Add config.ts

---
+++ packages/app/provider/config.ts
@@ -0,0 +1,34 @@
+type firebaseConfig = {
+  apiKey: string | undefined
+  authDomain: string | undefined
+  projectId: string | undefined
+  storageBucket: string | undefined
+  messagingSenderId: string | undefined
+  appId: string | undefined
+  measurementId: string | undefined
+}
+
+type Config = {
+  firebase: firebaseConfig
+  environment: string | undefined
+  emulatorHost: string | undefined
+  emulator: boolean
+  production: boolean
+}
+
+const config: Config = {
+  firebase: {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  },
+  environment: process.env.NEXT_PUBLIC_VERCEL_ENV ?? 'development',
+  emulatorHost: process.env.NEXT_PUBLIC_EMULATOR_HOST,
+  emulator: process.env.NEXT_PUBLIC_EMULATOR === 'true',
+  production: process.env.NODE_ENV === 'production',
+}
+export default config
